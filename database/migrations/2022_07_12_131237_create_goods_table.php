92205a652b511f9bbec13a3f9ee655dd80cea241
Marie Fujii <fujimari45@gmail.com>
2022-07-12 14:09:16 +0900

    ER図、マイグレーションファイルの作成

---
+++ database/migrations/2022_07_12_131237_create_goods_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('goods');
+            $table->integer('user_id');
+            $table->integer('status')->default('1');
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+            $table->timestamp('created_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+}
